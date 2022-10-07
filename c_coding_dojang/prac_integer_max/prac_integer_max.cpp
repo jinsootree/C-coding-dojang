963db34131186acc5073c0861a39b89866c43fb1
윤진수 <yjs104710@gmail.com>
2022-10-08 01:44:46 +0900

    commit

--- c_coding_dojang/prac_integer_max/prac_integer_max.cpp
+++ c_coding_dojang/prac_integer_max/prac_integer_max.cpp
@@ -1,15 +1,22 @@
 #include <stdio.h>
-/*(1)____________*/
+#include <limits.h> /*(1)____________*/
 
 int main()
 {
     char num1 = CHAR_MAX;
-    short num2 = /*(2)____*/;
+    short num2 = SHRT_MAX/*(2)____*/;
     int num3 = INT_MAX;
     long num4 = LONG_MAX;
-    long long num5 = /*(3)_____*/;
+    long long num5 = LLONG_MAX/*(3)_____*/;
 
     printf("%d %d %d %ld %lld\n", num1, num2, num3, num4, num5);
 
     return 0;
 }
+
+/*
+부호 있는 정수의 최댓값을 출력하는 문제입니다. 
+소스 코드의 CHAR_MAX, INT_MAX 등은 limits.h 헤더 파일에 정의되어 있으므로
+#include <limits.h>와 같이 헤더 파일을 포함해줍니다.
+그리고 short의 최댓값은 SHRT_MAX, long long의 최댓값은 LLONG_MAX입니다. 
+*/
--- c_coding_dojang/prac_integer_sizeof/prac_integer_sizof.cpp
+++ c_coding_dojang/prac_integer_sizeof/prac_integer_sizof.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    /*(1)___*/ num1;
-    /*(2)___*/ num2;
+    short /*(1)___*/ num1;
+    long long /*(2)___*/ num2;
 
     printf("%d\n", sizeof(num1) + sizeof(num2) + sizeof(int));
-
+    //2+8+4
     return 0;
 }
