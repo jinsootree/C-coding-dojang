dfac18dd1f3b8fdc9d80f9a64f77ee1068f9a8de
윤진수 <yjs104710@gmail.com>
2022-10-11 00:40:38 +0900

    'const'

--- c_coding_dojang/jud_const/jud_const.cpp
+++ c_coding_dojang/jud_const/jud_const.cpp
@@ -3,11 +3,17 @@
 
 int main()
 {
-    const /*_______________________________*/
-        const int con2 = 0x1285;
-    const long long con3 = 9223372036854775807LL;
+	const long double con1 = -2.225074e-308; /*_______________________________*/
+	const int con2 = 0x1285;
+	const long long con3 = 9223372036854775807LL;
 
-    printf("%Le 0x%x %lld\n", con1, con2, con3);
+	printf("%Le 0x%x %lld\n", con1, con2, con3);
 
-    return 0;
+	return 0;
 }
+/*"%Le 0x%x %lld\n"에서 서식 지정자 부분에
+con1, con2, con3을 넣어서 -2.225074e-308 0x1285 9223372036854775807를 출력하고 있습니다. 
+따라서 서식 지정자에 맞게 상수를 선언하고 값을 초기화하면 됩니다.
+
+%Le: 지수 표기법에 long double 크기이므로 
+const long double con1 = -2.225074e-308L과 같이 선언하고 초기화합니다.*/
--- c_coding_dojang/prac_const/prac_const.cpp
+++ c_coding_dojang/prac_const/prac_const.cpp
@@ -2,11 +2,18 @@
 
 int main()
 {
-    const /*(1)_________________________*/
-        const /*(2)_________________________*/
-        const /*(3)_________________________*/
+	const unsigned long long con1 = 10ULL;/*(1)_________________________*/
+	const float con2 = 3.2f;/*(2)_________________________*/
+	const char con3 = 't';/*(3)_________________________*/
 
-        printf("%llu %f %c\n", con1, con2, con3);
+	printf("%llu %f %c\n", con1, con2, con3);
 
-    return 0;
+	return 0;
 }
+/*"%llu %f %c\n"에서 서식 지정자 부분에 con1, con2, con3을
+넣어서 10 3.200000 t를 출력하고 있습니다.
+따라서 서식 지정자에 맞게 상수를 선언하고 값을 초기화하면 됩니다.
+
+%llu: unsigned long long 크기이므로 const unsigned long long con1 = 10ULL과 같이 선언하고 초기화합니다.
+%f: float 또는 double 크기이므로 const float con2 = 3.2f 또는 const double con2 = 3.2와 같이 선언하고 초기화합니다.
+%c: char 크기이므로 const char con3 = 't'와 같이 선언하고 초기화합니다.*/
