9d48b3a9b30333266b3172a4ad075c08c55a645f
윤진수 <yjs104710@gmail.com>
2022-10-13 19:28:39 +0900

    jud_inc_de

--- c_coding_dojang/jud_inc_de/jud_inc_de.cpp
+++ c_coding_dojang/jud_inc_de/jud_inc_de.cpp
@@ -9,10 +9,14 @@ int main()
 
     scanf("%d %f %c", &num1, &num2, &c1);
 
+    num1 = ++num1;
+    num2 = ++num2;
+    c1 = --c1;
+
     /*_____
     _______
     _____*/
-
+ 
     printf("%d %f %c\n", num1, num2, c1);
 
     return 0;
